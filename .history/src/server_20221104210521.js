09d7e8185a0895e497bdaac9986e87d97fdb91d7
dhchun1203 <dhchun1203@gmail.com>
2022-11-04 21:44:37 +0900

    #6.7, #6.8 Connecting to Mongo

--- .history/src/server_20221104210521.js
+++ .history/src/server_20221104210521.js
@@ -1,11 +1,11 @@
-import "./db";
-
 import express from "express";
 import morgan from "morgan";
 
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+
+import "./db";
 
 const PORT = 4000;
 console.log("current directory: ", process.cwd());
