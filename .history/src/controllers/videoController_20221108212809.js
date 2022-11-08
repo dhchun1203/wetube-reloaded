85ce5507e45d071af4f60e94ab30e8abda05e4f2
dhchun1203 <dhchun1203@gmail.com>
2022-11-08 21:31:55 +0900

    #6.19 Video Detail

--- .history/src/controllers/videoController_20221108212809.js
+++ .history/src/controllers/videoController_20221108212809.js
@@ -9,7 +9,7 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  return res.render("watch", { pageTitle: video.title, video });
+  return res.render("watch", { pageTitle: "Watching" });
 };
 
 export const getEdit = (req, res) => {
