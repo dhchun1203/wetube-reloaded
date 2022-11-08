c133e353e57f53b19754c0d6cd307364575b8ba5
dhchun1203 <dhchun1203@gmail.com>
2022-11-08 21:27:13 +0900

    #6.18 More Schema

--- .history/src/models/Video_20221108194935.js
+++ .history/src/models/Video_20221108194935.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    uppercase: true,
-    trim: true,
-    maxLength: 80,
-  },
-  description: { type: String, required: true, trim: true, maxLength: 20 },
+  title: { type: String, required: true, uppercase: true },
+  description: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String, trim: true }],
+  hashtags: [{ type: String }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
